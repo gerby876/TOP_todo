4c31c94cf3f1f4eb76965f7244e9177a437b2c27
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-04-09 18:35:21 -0400

    Work on add project button functionality style project cards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./style.css"
+
+const addproject = (function() {
+    const dialog = document.querySelector("dialog");
+    dialog.showModal();
+});
+
+const button = document.querySelector(".addp");
+
+button.addEventListener("click", () => {
+    addproject();
+});
